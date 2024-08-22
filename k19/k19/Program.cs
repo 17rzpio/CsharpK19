78aa293b778cd330760d12251d73c2ba7a2d6571
17rzpio <74218387+17rzpio@users.noreply.github.com>
2024-08-22 15:12:05 -0300

    Add files via upload
    curso de c# da apostila da k19

---
+++ k19/k19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int count = 0;
+        while (count < 100)
+        {
+            Console.WriteLine("hello world");
+            count++;
+        }
+        for(count=0;count<100;count++)
+            Console.WriteLine("hello world");
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\n*\n**\n***");
+
+        for(int count2 = 0; count2 < 100; count2++)
+        {
+            Console.WriteLine("Rodrigo Zaleski Pioner");
+            Console.WriteLine(count2);
+        }
+        for(int count3 = 0; count3 < 100; count3++)
+        {
+            if(count3%2!=0)
+                Console.WriteLine(  "*");
+            else
+                Console.WriteLine(  "**");
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
